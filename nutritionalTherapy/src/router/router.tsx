f8204a5e8451648dfc77ce2ca5b5465d96c0177a
Eduardo Graciano <>
2022-09-05 16:14:21 +0200

    base navigation and screens

---
+++ nutritionalTherapy/src/router/router.tsx
@@ -0,0 +1,55 @@
+import {
+  NativeStackScreenProps,
+  createNativeStackNavigator,
+} from '@react-navigation/native-stack';
+import React from 'react';
+import { DetailsPage } from '../details/DetailsPage';
+import { HomePage } from '../home/HomePage';
+import { SplashPage } from '../splash/SplashPage';
+
+export const RouteName = {
+  splash: 'Splash',
+  home: 'Home',
+  details: 'Details',
+} as const;
+
+export type StackParams = {
+  [RouteName.splash]: undefined;
+  [RouteName.home]: undefined;
+  [RouteName.details]: undefined;
+};
+
+export type NavigationProps = {
+  [RouteName.splash]: NativeStackScreenProps<
+    StackParams,
+    'Splash'
+  >['navigation'];
+  [RouteName.home]: NativeStackScreenProps<StackParams, 'Home'>['navigation'];
+  [RouteName.details]: NativeStackScreenProps<
+    StackParams,
+    'Details'
+  >['navigation'];
+};
+
+const Stack = createNativeStackNavigator<StackParams>();
+const mainOptions = {
+  headerShown: false,
+};
+
+type ExpensesRoutesProps = {
+  initialScreen?: keyof typeof RouteName;
+};
+
+export const ExpensesRoutes = ({
+  initialScreen = 'splash',
+}: ExpensesRoutesProps) => {
+  return (
+    <Stack.Navigator
+      screenOptions={mainOptions}
+      initialRouteName={RouteName[initialScreen]}>
+      <Stack.Screen name={RouteName.splash} component={SplashPage} />
+      <Stack.Screen name={RouteName.home} component={HomePage} />
+      <Stack.Screen name={RouteName.details} component={DetailsPage} />
+    </Stack.Navigator>
+  );
+};
